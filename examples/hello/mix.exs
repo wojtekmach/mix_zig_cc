9550de1b0f741dea4e5277d53b5f25f016904215
Wojtek Mach <wojtek@wojtekmach.pl>
2021-05-15 11:03:24 +0200

    Download zig if not present

--- examples/hello/mix.exs
+++ examples/hello/mix.exs
@@ -32,6 +32,18 @@ defmodule Hello.MixProject do
     []
   end
 
+  def target() do
+    case :string.split(:erlang.system_info(:system_architecture), '-', :all) do
+      [cpu, _vendor, os | _] ->
+        os = if List.starts_with?(os, 'darwin'), do: 'macos', else: os
+        cpu = if os == 'macos' and List.starts_with?(cpu, 'arm'), do: 'aarch64', else: cpu
+        "#{cpu}-#{os}"
+
+      ['win32'] ->
+        "x86_64-windows"
+    end
+  end
+
   defp zig_cc(_) do
     zig_cc()
   end
@@ -42,7 +54,7 @@ defmodule Hello.MixProject do
     {"x86_64", "macos"}
   ]
 
-  def zig_cc do
+  defp zig_cc do
     input = "c_src/hello_nif.c"
     output = "priv/hello_nif"
 
@@ -82,19 +94,41 @@ defmodule Hello.MixProject do
     if not force and File.exists?(output) do
       "echo #{output} already exists"
     else
-      "echo building #{output}; #{context.env}zig cc #{input} #{opts}"
+      "echo building #{output}; #{context.env}#{zig()} cc #{input} #{opts}"
     end
   end
 
-  def target() do
-    case :string.split(:erlang.system_info(:system_architecture), '-', :all) do
-      [cpu, _vendor, os | _] ->
-        os = if List.starts_with?(os, 'darwin'), do: 'macos', else: os
-        cpu = if os == 'macos' and List.starts_with?(cpu, 'arm'), do: 'aarch64', else: cpu
-        "#{cpu}-#{os}"
+  defp zig() do
+    if System.find_executable("zig") && false do
+      "zig"
+    else
+      version = "0.8.0-dev.2237+8eea5eddf"
+      cache_dir = cache_dir()
+      [arch, os] = String.split(target(), "-")
+      target = "#{os}-#{arch}"
+      name = "zig-#{target}-#{version}"
 
-      ['win32'] ->
-        "x86_64-windows"
+      bin = Path.join([cache_dir, name, "zig"])
+
+      if File.exists?(bin) do
+        bin
+      else
+        url = "https://ziglang.org/builds/#{name}.tar.xz"
+        path = Path.join(cache_dir, Path.basename(url))
+        File.mkdir_p!(cache_dir)
+        Mix.shell().info("downloading #{url} to #{path}")
+        0 = Mix.shell().cmd("curl --fail #{url} > #{path}")
+        0 = Mix.shell().cmd("tar xf #{path}", cd: cache_dir)
+        bin
+      end
+    end
+  end
+
+  defp cache_dir() do
+    if System.get_env("MIX_XDG") in ["1", "true"] do
+      :filename.basedir(:user_cache, "mix_zig_cc", %{os: :linux})
+    else
+      :filename.basedir(:user_cache, "mix_zig_cc")
     end
   end
 end
